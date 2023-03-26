c183914b4bf70e4d08e53fba0d6fd3cf43fbbec7
Demitri <61719255+Hari137@users.noreply.github.com>
2023-03-26 18:05:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var movementStrength = 25;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $("div").mousemove(function(e){
+              var pageX = e.pageX - ($(window).width() / 2);
+              var pageY = e.pageY - ($(window).height() / 2);
+              var newvalueX = width * pageX * -1 - 25;
+              var newvalueY = height * pageY * -1 - 50;
+              $('div').css("background", newvalueX+"px     "+newvalueY+"px");
+    });
+    });
